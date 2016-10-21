75be014be7919704c4d77b58573c1b7dec9cb680
Maximiliano Pombo <maxipombo@gmail.com>
2016-10-21 20:56:41 -0300

    Initial commit with Firebase configuration

---
+++ app/src/main/java/uy/com/siennadesigns/la_luz/MainActivity.java
@@ -0,0 +1,13 @@
+package uy.com.siennadesigns.la_luz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
